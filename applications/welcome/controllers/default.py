d667867e8621eb51abdda23a1a40da4f99b7e862
maslick <pavel.masloff@gmail.com>
2019-11-20 18:26:42 +0100

    added simple business logic

--- applications/welcome/controllers/default.py
+++ applications/welcome/controllers/default.py
@@ -1,2 +1,22 @@
+from quote import quote
+
+
 def index():
-    return "hello world"
+    resp = dict()
+    resp["welcome"] = 'Welcome to web2py!'
+    resp["message"] = 'Hello World!!!'
+    return resp
+
+
+def quotes():
+    resp = dict()
+
+    citations = []
+    for q in quote('Pushkin', limit=5):
+        row = dict()
+        row["author"] = q["author"]
+        row["quote"] = q["quote"]
+        citations.append(row)
+
+    resp["citations"] = citations
+    return resp
